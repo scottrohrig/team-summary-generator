e37044a821f88a4d0c58091405a5a6ab3798e6d1
scottrohrig <scott.rohrig@gmail.com>
2022-01-08 15:06:32 -0800

    add first pass file writing module

---
+++ src/generate-file.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+const writeFile = fileContent => new Promise( ( resolve, reject ) => {
+    fs.writeFile( '../dist/team-summary.html', JSON.stringify( fileContent ), ( err ) => {
+        if ( err ) throw err;
+        console.log( 'Summary created! Check ./dist/team-summary.html for verification.' );
+    } )
+} );
+
+export default writeFile;
